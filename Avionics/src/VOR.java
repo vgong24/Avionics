9932954e35dd9d0e59a6d952b468fdc6b9f4f395
Victor Gong <vgong24@gmail.com>
2014-03-18 13:34:08 -1000

    added fields and JUnit file
    This is what I 'think' the VOR has/needs

--- Avionics/src/VOR.java
+++ Avionics/src/VOR.java
@@ -5,4 +5,43 @@ public class VOR{
 		System.out.println("Hello World");
 		//remoted test line
 	}
+	
+	//VOR ELEMENTS
+	/*
+	 * int degrees
+	 * Sets the direction of where the VOR is pointing to
+	 * Gets degrees from the dial
+	 */
+	int degrees;
+	
+	/*
+	 * boolean to/from
+	 * Where the object is relative to VOR
+	 * If the plane is on the opposite direction of the perpendicular sides of the degrees
+	 * Then it is 'to' else 'from'
+	 */
+	boolean isTo;
+	
+	//Coordinates of VOR
+	int[] coord;
+	
+	/**
+	 * Constructor
+	 */
+	public VOR(int posx, int posy){
+		degrees = 0;
+		isTo = true;
+		coord = new int[2];
+		coord[0]= posx;
+		coord[1]= posy;
+	}
+	/**
+	 * Get the radio signal/dial from the plane
+	 * input is stored in VOR
+	 * @param degree
+	 */
+	public void radioDegree(int degree){
+		degrees = degree;
+	}
+	
 }
---
+++ Avionics/src/VORTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class VORTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVOR() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRadioDegree() {
+		fail("Not yet implemented");
+	}
+
+}
